70b7d54b6dc1eedccc5d636a9b65c056125c9006
Jlorden <Jamie.lorden@gmail.com>
2020-02-18 13:29:56 -0600

    Daily Algos

--- 4_extra_practice.js
+++ 4_extra_practice.js
@@ -2,4 +2,19 @@
 // I.E.  If you wrote both of your algorithms using for loops, now write them out with while loops
 
 
-// YOUR CODE HERE
+var i = 0;
+while(i < 20){
+    if(i % 2!== 0) {
+        console.log(i)
+    }
+        i++;
+}
+
+var num = 1
+var sum = 1
+while(num < 10) {
+    num++;
+    console.log(num)
+    sum = sum + num;
+    console.log(sum)
+}
